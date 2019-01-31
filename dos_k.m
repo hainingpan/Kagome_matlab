a0507b44c524c1af8d8f4bd78e4f0d2ef8696489
Haining Pan <jack_pan1994@126.com>
2019-01-31 00:19:41 -0500

    update ldos, example_ldos

--- dos_k.m
+++ dos_k.m
@@ -12,9 +12,4 @@ ham=1/(2*mu)*eye((2*Nmax+1)^2).*(tmp4*tmp4')+offdiag;
 enlist=eig(ham);
 [omegagrid,engrid]=meshgrid(omega,enlist);
 re=-imag(sum(1./((omegagrid+1i*delta)-engrid),1))/pi;
-% re=-imag(sum(1./((omega+1i*delta)-enlist)))/pi;
-
-% G=inv((omega+1i*delta)*eye((2*Nmax+1)*(2*Nmax+1))-ham);
-% % G=sparseinv()
-% re=-imag(trace(G))/pi;
 end
---
+++ example_ldos.m
@@ -0,0 +1,14 @@
+energylist=-3e-3:0.1e-3:7e-3;
+delta=1e-5;
+latticeconst=sqrt(parameters.cellnumber)*(2)/parameters.NN;
+for i=1:length(energylist)
+    disp(i);
+    omega=energylist(i);
+    ldosstoremat=ldos(omega,delta,parameters);
+    save(strcat('.\data\555\ldos_e',num2str(omega),'.dat'),'ldosstoremat','-ascii');
+    figure;
+    surf((1:parameters.NN)*sqrt(parameters.cellnumber)*(2)/parameters.NN,(1:parameters.NN)*sqrt(parameters.cellnumber)*(2)/parameters.NN,ldosstoremat,'edgecolor','none');view(2);colorbar
+    title(strcat('E=',num2str(omega),'(meV)'));
+    axis tight;
+    saveas(gcf,strcat('.\data\fig\555\ldos_e',num2str(omega),'.png'));
+end
