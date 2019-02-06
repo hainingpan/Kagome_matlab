89b16a3b9a7f047c0d6c4f44af9e97cc7ee120d0
Haining Pan <jack_pan1994@126.com>
2019-02-05 22:56:03 -0500

    diagonal of sparse matrix, by LU decomposition and Takahashi equation
    https://docs.lib.purdue.edu/cgi/viewcontent.cgi?referer=https://www.google.com/&httpsredir=1&article=1335&context=nanopub
    
    http://delivery.acm.org/10.1145/370000/360704/p177-erisman.pdf?ip=129.2.40.228&id=360704&acc=ACTIVE%20SERVICE&key=5F8E7AA76238C9EB%2E737F10F6E50A862B%2E4D4702B0C3E38B35%2E4D4702B0C3E38B35&__acm__=1549382078_eaa87aaaa387d81799d3d7d1f239fb3c

--- ldos.m
+++ ldos.m
@@ -5,6 +5,6 @@ ham=Htb(parameters);
 K=(omega+1i*delta)*speye(NN^2)-ham;
 % G=inv(full((omega+1i*delta)*speye(NN^2)-ham));
 G=spinv(K);
-Gdiag=diag(G);
-re=-1/pi*imag(reshape(Gdiag,NN,NN));
+% Gdiag=diag(G);
+re=-1/pi*imag(reshape(G,NN,NN));
 end
---
+++ zfun.m
@@ -0,0 +1,50 @@
+function [z,cal]=zfun(i,j,z,cal,C,L,d,U)
+n=length(cal);
+if cal(i,j)~=1
+    %diagonal
+    if (i==j)
+    c=C(i,i+1:n);
+    c=sparse((i+1:n)',ones(n-i,1),c',n,1);
+%     c=padarray(c,[0,i],'pre');
+    citer=1:n;
+    citer=citer(logical(c));
+    for k=citer
+        [z,cal]=zfun(k,i,z,cal,C,L,d,U);
+    end
+    z(i,j)=1/d(i)-U(i,c)*z(c,i);
+    cal(i,j)=1;
+    re=z(i,j);
+    else
+        %upper tri
+        if (i<j)
+            c=C(i,i+1:n);
+            c=sparse((i+1:n)',ones(n-i,1),c',n,1);
+%             c=padarray(c,[0,i],'pre');
+            citer=1:n;
+            citer=citer(logical(c));
+            for k=citer
+                [z,cal]=zfun(k,i,z,cal,C,L,d,U);
+            end
+            z(i,j)=-U(i,c)*z(c,j);
+            cal(i,j)=1;
+            re=z(i,j);
+        else
+            %lower tri
+            c=C(i,j+1:n);
+            c=sparse((j+1:n)',ones(n-j,1),c',n,1);
+%             c=padarray(c,[0,j],'pre');
+            citer=1:n;
+            citer=citer(logical(c));
+            for k=citer
+                [z,cal]=zfun(i,k,z,cal,C,L,d,U);
+            end
+            z(i,j)=-z(i,c)*L(c,j);            
+            cal(i,j)=1;
+            re=z(i,j);
+        end
+    end
+end
+end
+                
+        
+    
