d52a78352965fc13da26087d24a0431fba18d5c4
Haining Pan <jackpan1994@gmail.com>
2019-05-06 12:50:30 -0400

    add LDOS in r

--- ldos_k.m
+++ ldos_k.m
@@ -8,14 +8,22 @@ vecf(:,:,i)=fftshift(vecf(:,:,i));
 end
 
 enlist=-5e-3:0.05e-3:5e-3;
-enmap=zeros(sqrt(length(h)),sqrt(length(h)),length(enlist));
+enmapk=zeros(sqrt(length(h)),sqrt(length(h)),length(enlist));
+enmapr=zeros(sqrt(length(h)),sqrt(length(h)),length(enlist));
 
 delta=1e-4;
 for i=1:length(enlist)
 deltaf=reshape(delta./((enlist(i)-val).^2+delta^2),[1,1,length(h)]);
 psif=abs(vecf).^2;
-enmap(:,:,i)=sum(deltaf.*psif,3);
+enmapk(:,:,i)=sum(deltaf.*psif,3);
 end
-figure;surf(sqrt(3)*(-floor(parameters.NN/2):floor((parameters.NN-1)/2))/(2*sqrt(parameters.cellnumber)),enlist,squeeze(log(enmap(:,parameters.NN/2,:)))','edgecolor','none');view(2);
+
+for i=1:length(enlist)
+deltaf=reshape(delta./((enlist(i)-val).^2+delta^2),[1,1,length(h)]);
+psi=abs(vec2).^2;
+enmapr(:,:,i)=sum(deltaf.*psi,3);
+end
+
+figure;surf(sqrt(3)*(-floor(parameters.NN/2):floor((parameters.NN-1)/2))/(2*sqrt(parameters.cellnumber)),enlist,squeeze(log(enmapk(:,parameters.NN/2,:)))','edgecolor','none');view(2);
 xlim([-4,4]);
 
